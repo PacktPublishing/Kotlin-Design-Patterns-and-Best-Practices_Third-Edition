3fb6f9112cf974e4c46fd1ca204b9dc5aeec8099
AlexeySoshin <alexey.soshin@gmail.com>
2023-09-30 22:01:36 +0100

    Update Gradle version for Chapter 7

--- Chapter07/build.gradle.kts
+++ Chapter07/build.gradle.kts
@@ -1,21 +1,17 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.9.10"
 }
 
 group = "me.soshin"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(11)
 }
