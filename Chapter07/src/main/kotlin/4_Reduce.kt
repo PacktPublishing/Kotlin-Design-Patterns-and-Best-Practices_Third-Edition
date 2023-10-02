4edc558eba6f75e6b78effd5db692b8417512619
AlexeySoshin <alexey.soshin@gmail.com>
2023-10-02 15:12:01 +0100

    Added flow sharing and advanced operations

--- Chapter07/src/main/kotlin/4_Reduce.kt
+++ Chapter07/src/main/kotlin/4_Reduce.kt
@@ -1,12 +1,44 @@
-fun main() {
-    val numbers = 1..100
+import kotlinx.coroutines.runBlocking
 
+fun main() {
+    reduce()
+    fold()
+    scan()
+}
+
+fun reduce() {
+    val numbers = 1..100
+    reduceImperative(numbers)
+    reduceReactive(numbers)
+}
+
+fun reduceReactive(numbers: IntRange) {
+    val reduced: Int = (1..100).reduce { sum, n -> sum + n }
+    println("reduced: $reduced")
+}
+
+fun fold() {
+    val folded: Int = (1..100).fold(10) { sum, n -> sum + n }
+    println("folded: $folded")
+
+    val foldedLong: Long = (1..15).fold(1L) { acc, n -> acc * n }
+    println("foldedLong: $foldedLong")
+}
+
+fun reduceImperative(numbers: IntRange) {
     var sum = 0
     for (n in numbers) {
         sum += n
     }
     println(sum)
+}
 
-    val reduced: Int = (1..100).reduce { sum, n -> sum + n }
-    println(reduced)
+fun scan() {
+    runBlocking {
+        val reduced: Int = (1..100).reduce { sum, n -> sum + n }
+        println("reduced: $reduced")
+
+        val scanned: List<Int> = (1..100).scan(0) { sum, n -> sum + n }
+        println("scanned: $scanned")
+    }
 }
