e18bd94d3a694716379b0c8a0c3acea5948560cf
Alexey Soshin <alexey.soshin@gmail.com>
2023-11-17 11:29:41 +0000

    Optics example

--- Chapter09/build.gradle.kts
+++ Chapter09/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.20"
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
     application
 }
 
@@ -18,6 +19,8 @@ dependencies {
     implementation("io.arrow-kt:arrow-fx-coroutines:1.2.1")
     implementation("io.arrow-kt:arrow-resilience-jvm:1.2.1")
     implementation("io.arrow-kt:arrow-fx-stm-jvm:1.2.1")
+    implementation("io.arrow-kt:arrow-optics:1.2.1")
+    ksp("io.arrow-kt:arrow-optics-ksp-plugin:1.2.1")
     testImplementation(kotlin("test"))
 }
 
